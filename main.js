820934c3278390f132534250bcadc37b41b21e61
rgnji <xx141463.c@nycu.edu.tw>
2024-07-27 19:20:28 +0800

    vue test

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function select(target) {
+    const icon = target.getElementsByTagName('img')[0];
+    const bg = target.getElementsByTagName('img')[1];
+    const fr = target.getElementsByTagName('img')[2];
+    
+}
