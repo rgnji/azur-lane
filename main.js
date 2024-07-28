12ad85fc2d128f48aaa630803a4c5724e81528a6
rgnji <xx141463.c@nycu.edu.tw>
2024-07-29 01:41:07 +0800

    next

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-
-function select(target) {
-    const icon = target.getElementsByTagName('img')[0];
-    const bg = target.getElementsByTagName('img')[1];
-    const fr = target.getElementsByTagName('img')[2];
-    
-}
+const app = Vue.createApp({});
+app.component("ship-select",
+    template:
+);
