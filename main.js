1ad8906188d0c573380f33b5d8acac92053d4e00
rgnji <xx141463.c@nycu.edu.tw>
2024-07-31 04:08:09 +0800

    next change button img

--- main.js
+++ main.js
@@ -1,4 +1,55 @@
-const app = Vue.createApp({});
-app.component("ship-select",
-    template:
-);
+import { shiplist } from "./ship.js";
+import { weaponlist } from "./weapon.js";
+
+const select_ship = Vue.createApp({
+  data() {
+    return {
+      shiplist: shiplist,
+    };
+  },
+});
+
+select_ship.component("ship-select", {
+  props: ["name", "bg", "fr", "icon"],
+  template: `
+            <div class="col-md-auto px-1">
+              <div class="card card_style">
+                <button class="item_container" data-bs-dismiss="modal">
+                  <img class="bg" :src="bg" />
+                  <img class="fr" :src="fr" />
+                  <img class="icon" :src="icon" />
+                </button>
+                <div class="card-body p-1">
+                  <p class="card-text">{{name}}</p>
+                </div>
+              </div>
+            </div>`,
+});
+
+select_ship.mount("#ship_select");
+
+const select_weapon = Vue.createApp({
+  data() {
+    return {
+      weaponlist: weaponlist,
+    };
+  },
+});
+
+select_weapon.component("weapon-select", {
+  props: ["name", "bg", "fr", "icon"],
+  template: `<div class="col-md-auto px-1">
+              <div class="card card_style">
+                <button class="item_container" data-bs-dismiss="modal">
+                  <img class="bg" :src="bg" />
+                  <img class="fr" :src="fr" />
+                  <img class="icon" :src="icon" />
+                </button>
+                <div class="card-body p-1">
+                  <p class="card-text">{{name}}</p>
+                </div>
+              </div>
+            </div>`,
+});
+
+select_weapon.mount("#weapon_select")
---
+++ weapon.js
@@ -0,0 +1,10 @@
+export const weaponlist = {
+  weapon1: {
+    name: "406mm三連装砲MK7T0",
+    bg: "ui/bg5.png",
+    fr: "ui/frame_5.png",
+    icon: "equips/large/14400.png",
+    type: "large",
+    rarity: "UR",
+  },
+};
